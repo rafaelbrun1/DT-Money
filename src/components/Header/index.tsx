88479b8f378a80162386df60425cb09fb075bd1d
Rafael Brun <rafael.brun@yahoo.com.br>
2022-11-17 13:20:09 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import logoImg from "../../assets/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
+
+export function Header() {
+  return (
+    <div>
+      <HeaderContainer>
+        <HeaderContent>
+          <img src={logoImg} alt="" />
+
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <NewTransactionButton>Nova Transação</NewTransactionButton>
+            </Dialog.Trigger>
+
+            <NewTransactionModal />
+          </Dialog.Root>
+        </HeaderContent>
+      </HeaderContainer>
+    </div>
+  );
+}
